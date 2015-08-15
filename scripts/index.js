9e61df71bc47a698ebb5645dceb7c2002745fb27
AlkalizZ <799478793@qq.com>
2015-08-15 17:20:39 +0800

    改进

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,28 @@
 /**
  * Created by Alkali on 15/8/15.
  */
-var checkBox = document.getElementsByName("checkbox"),
-    selectAllBtn = document.getElementById("selectAll"),
-    reversedBtn = document.getElementById("reversed");
-
-var Check = {
-    selectAll: function(){
-        for(var i = 0; i < checkBox.length; i++){
-            checkBox[i].checked = "true";
+;(function(){
+    MyQuery.addLoadHandler(function(){
+        var checkBox = document.getElementsByName("checkbox"),
+            selectAllBtn = document.getElementById("selectAll"),
+            reversedBtn = document.getElementById("reversed");
+        var Check = {
+            selectAll: function(){
+                for(var i = 0; i < checkBox.length; i++){
+                    checkBox[i].checked = "true";
+                }
+            },
+            reversed: function(){
+                for(var i = 0; i < checkBox.length; i++){
+                    checkBox[i].checked ? checkBox[i].checked = false : checkBox[i].checked = true;
+                }
+            }
         }
-    },
-    reversed: function(){
-        for(var i = 0; i < checkBox.length; i++){
-            checkBox[i].checked ? checkBox[i].checked = false : checkBox[i].checked = true;
-        }
-    }
-}
-
-MyQuery.addHandler(selectAllBtn, "click", function(){
-    Check.selectAll();
-});
-MyQuery.addHandler(reversedBtn, "click", function(){
-    Check.reversed();
-});
+        MyQuery.addHandler(selectAllBtn, "click", function(){
+            Check.selectAll();
+        });
+        MyQuery.addHandler(reversedBtn, "click", function(){
+            Check.reversed();
+        });
+    });
+})();
