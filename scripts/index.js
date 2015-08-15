8a7ee7e9e23d9bbf01b37a459541025d5b5d35cb
AlkalizZ <799478793@qq.com>
2015-08-16 01:31:35 +0800

    改要求后。。

--- scripts/index.js
+++ scripts/index.js
@@ -2,27 +2,38 @@
  * Created by Alkali on 15/8/15.
  */
 ;(function(){
-    MyQuery.addLoadHandler(function(){
-        var checkBox = document.getElementsByName("checkbox"),
-            selectAllBtn = document.getElementById("selectAll"),
-            reversedBtn = document.getElementById("reversed");
-        var Check = {
-            selectAll: function(){
-                for(var i = 0; i < checkBox.length; i++){
-                    checkBox[i].checked = "true";
-                }
-            },
-            reversed: function(){
-                for(var i = 0; i < checkBox.length; i++){
-                    checkBox[i].checked ? checkBox[i].checked = false : checkBox[i].checked = true;
-                }
+    var checkBox = document.querySelectorAll("input[name='checkbox']"),
+        selectBtn = document.querySelector("#selectAll"),
+        reversedBtn = document.querySelector("#reversed"),
+        allBtn = document.querySelectorAll("input");
+    selectBtn.addEventListener("click", function(){
+        if(selectBtn.checked){
+            for(var i = 0; i < checkBox.length; i++){
+                checkBox[i].checked = true;
+            }
+            console.log(false);
+
+        }else{
+            for(var i = 0; i < checkBox.length; i++){
+                checkBox[i].checked = false;
             }
         }
-        MyQuery.addHandler(selectAllBtn, "click", function(){
-            Check.selectAll();
-        });
-        MyQuery.addHandler(reversedBtn, "click", function(){
-            Check.reversed();
-        });
-    });
+    }, false);
+    reversedBtn.addEventListener("click", function(){
+        for(var i = 0; i < checkBox.length; i++){
+            checkBox[i].checked ? checkBox[i].checked = false : checkBox[i].checked = true;
+        }
+    }, false);
+
+    for(var i = 0; i < allBtn.length; i++){
+        allBtn[i].addEventListener("click", function(){
+            var arr = 0;
+            for(var i = 0; i < checkBox.length; i++){
+                if(checkBox[i].checked){
+                    arr++;
+                }
+            }
+            arr == checkBox.length ? selectBtn.checked = true : selectBtn.checked = false;
+        }, false);
+    }
 })();
